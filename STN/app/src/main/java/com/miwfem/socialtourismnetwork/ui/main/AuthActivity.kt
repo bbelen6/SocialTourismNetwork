60b917477ddf8c45c6e4121052419f8d8d286ac2
Belen Garcia <bbelengarcia6@gmail.com>
2021-05-09 20:32:42 +0200

    Authentication and logout screens created issue#4

---
+++ STN/app/src/main/java/com/miwfem/socialtourismnetwork/ui/main/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.miwfem.socialtourismnetwork.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miwfem.socialtourismnetwork.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
