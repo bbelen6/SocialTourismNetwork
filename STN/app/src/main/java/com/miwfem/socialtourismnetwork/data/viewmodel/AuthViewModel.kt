28561ea342213b05243cbb56e8990ddf178c75e6
Belen Garcia <bbelengarcia6@gmail.com>
2021-05-23 13:01:55 +0200

    refactor show alert function to reuse

--- STN/app/src/main/java/com/miwfem/socialtourismnetwork/data/viewmodel/AuthViewModel.kt
+++ STN/app/src/main/java/com/miwfem/socialtourismnetwork/data/viewmodel/AuthViewModel.kt
@@ -1,7 +1,27 @@
 package com.miwfem.socialtourismnetwork.data.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
 
 class AuthViewModel : ViewModel() {
-    val prueba = "PRUEBA"
+
+    fun singUp(email: String, password: String): Boolean {
+        var resultSingUp = false
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+            email, password
+        ).addOnCompleteListener {
+            resultSingUp = it.isSuccessful
+        }
+        return resultSingUp
+    }
+
+    fun login(email: String, password: String): Boolean {
+        var resultLogin = false
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+            email, password
+        ).addOnCompleteListener {
+            resultLogin = it.isSuccessful
+        }
+        return resultLogin
+    }
 }
--- STN/app/src/main/java/com/miwfem/socialtourismnetwork/ui/auth/AuthFragment.kt
+++ STN/app/src/main/java/com/miwfem/socialtourismnetwork/ui/auth/AuthFragment.kt
@@ -3,15 +3,11 @@ package com.miwfem.socialtourismnetwork.ui.auth
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import com.google.firebase.auth.FirebaseAuth
 import com.miwfem.socialtourismnetwork.R
 import com.miwfem.socialtourismnetwork.data.viewmodel.AuthViewModel
 import com.miwfem.socialtourismnetwork.databinding.FragmentAuthBinding
 import com.miwfem.socialtourismnetwork.ui.base.BaseFragment
-import com.miwfem.socialtourismnetwork.ui.home.HomeFragment
 import com.miwfem.socialtourismnetwork.ui.main.MainActivity
-import com.miwfem.socialtourismnetwork.utils.TAG_HOME
-import kotlinx.android.synthetic.main.fragment_auth.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class AuthFragment : BaseFragment(R.layout.fragment_auth) {
@@ -36,53 +32,59 @@ class AuthFragment : BaseFragment(R.layout.fragment_auth) {
                 (requireActivity() as? MainActivity)?.onBackPressed()
             }
             singUpButton.setOnClickListener {
-                if (emailEditText.text.isNotEmpty() && password_edit_text.text.isNotEmpty()) {
-                    FirebaseAuth.getInstance().createUserWithEmailAndPassword(
-                        emailEditText.text.toString(),
-                        passwordEditText.text.toString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            showHome(it.result?.user?.email ?: "", "HOME")
-                        } else {
-                            showAlert()
-                        }
+                if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                    if (authViewModel.singUp(
+                            emailEditText.text.toString(),
+                            passwordEditText.text.toString()
+                        )
+                    ) {
+                        (requireActivity() as? MainActivity)?.onBackPressed()
+                    } else {
+                        showAlert(
+                            title = getString(R.string.error),
+                            message = getString(R.string.sing_up_error),
+                            positiveButton = getString(R.string.accept)
+                        )
                     }
+                } else {
+                    showAlert(
+                        message = getString(R.string.fill_error),
+                        positiveButton = getString(R.string.accept)
+                    )
                 }
             }
-
             loginButton.setOnClickListener {
-                if (emailEditText.text.isNotEmpty() && password_edit_text.text.isNotEmpty()) {
-                    FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                        emailEditText.text.toString(),
-                        passwordEditText.text.toString()
-                    ).addOnCompleteListener {
-                        if (it.isSuccessful) {
-                            showHome(it.result?.user?.email ?: "", "Home")
-                        } else {
-                            showAlert()
-                        }
+                if (emailEditText.text.isNotEmpty() && passwordEditText.text.isNotEmpty()) {
+                    if (authViewModel.login(
+                            emailEditText.text.toString(),
+                            passwordEditText.text.toString()
+                        )
+                    ) {
+                        (requireActivity() as? MainActivity)?.onBackPressed()
+                    } else {
+                        showAlert(
+                            title = getString(R.string.error),
+                            message = getString(R.string.login_error),
+                            positiveButton = getString(R.string.accept)
+                        )
                     }
+                } else {
+                    showAlert(
+                        message = getString(R.string.fill_error),
+                        positiveButton = getString(R.string.accept)
+                    )
                 }
             }
         }
     }
 
-    private fun showAlert() {
+    private fun showAlert(title: String = "", message: String = "", positiveButton: String = "") {
         val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("Error")
-            .setMessage("Se ha producido un error en la autenticación del usuario")
-            .setPositiveButton("Aceptar", null)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton(positiveButton, null)
         val dialog = builder.create()
         dialog.show()
-    }
-
-    private fun showHome(email: String, provider: String) {
-        (requireActivity() as? MainActivity)?.addFragment(
-            HomeFragment.newInstance(
-                email,
-                provider
-            ), R.id.fragmentComplete, TAG_HOME
-        )
     }
 
     override fun handleOnBackPressed(): Boolean {
